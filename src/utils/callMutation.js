4a09c4cc01949756151d17d3ba22ef5a0d815f33
NutthakornManeewan <nutthakorn.m@vsni.co.uk>
2020-03-02 16:19:48 +0700

    :fire: Test create field API call.

--- src/utils/callMutation.js
+++ src/utils/callMutation.js
@@ -26,6 +26,40 @@ const CREATE_USER = gql`
 	}
 `;
 
+const CREATE_FIELD = gql`
+	mutation create_field(
+		$field_name: String!
+		$field_area: Float!
+		$field_owner: String!
+		$field_location: String!
+		$field_description: String
+		$field_soil_type: String
+		$field_nitrogen_quantity: String
+		$field_phosphorus_quantity: String
+		$field_potassium_quantity: String
+		$field_ph: Float
+	) {
+		create_field(
+			field_ph: $field_ph
+			field_name: $field_name
+			field_area: $field_area
+			field_location: $field_location
+			field_description: $field_description
+			field_soil_type: $field_soil_type
+			field_nitrogen_quantity: $field_nitrogen_quantity
+			field_phosphorus_quantity: $field_phosphorus_quantity
+			field_potassium_quantity: $field_potassium_quantity
+			field_owner: $field_owner
+		) {
+			_id
+			field_name
+			field_owner {
+				_id
+			}
+		}
+	}
+`;
+
 export const callMutation = async args => {
 	console.log("args ==>", args);
 	const { user_name, user_email, user_phone, user_address } = { ...args };
@@ -38,12 +72,18 @@ export const callMutation = async args => {
 				Accept: "*/*"
 			},
 			data: {
-				query: print(CREATE_USER),
+				query: print(CREATE_FIELD),
 				variables: {
-					user_name,
-					user_email,
-					user_address,
-					user_phone
+					field_ph: 7,
+					field_name: "Pareena field",
+					field_area: 15,
+					field_location: "5e4f4b5652ceaa70e94c14a6",
+					field_description: "This is Pareena field",
+					field_soil_type: "SANDY",
+					field_nitrogen_quantity: "MEDIUM",
+					field_phosphorus_quantity: "HIGH",
+					field_potassium_quantity: "LOW",
+					field_owner: "5e4f3d2172f987507bc9c677"
 				}
 			}
 		});
